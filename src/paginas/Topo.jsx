06c94659fe3643c05e2e0793f9978245fea6165d
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-09 00:35:26 -0300

    alterado

--- src/paginas/Topo.jsx
+++ src/paginas/Topo.jsx
@@ -1,10 +1,10 @@
-import Img from '../assets/bg_gisa.png'
+// import Img from '../assets/bg_gisa.png'
 
 export function Topo(){
     return (
         <>
         <div className='dados margin'>
-            <img src={Img} alt='' />
+            <img src="" alt='' />
             <div className='info'>
                <h1>Gisa Gourmeteria</h1>
                <p>Estrada Adhemar Bebiano, 3546 - Inhaúma - RJ</p>
