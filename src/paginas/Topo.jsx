dad7e3b25efd3f052d66d3bf7ad63bcca1ef2a06
Antonio Santos <84925495+tonydesigner@users.noreply.github.com>
2023-07-09 02:03:34 -0300

    atualiza topo

--- src/paginas/Topo.jsx
+++ src/paginas/Topo.jsx
@@ -7,7 +7,9 @@ export function Topo(){
             <img src="https://img.elo7.com.br/product/original/31FC62E/logo-para-confeitaria-logo-confeitaria.jpg" alt='' />
             <div className='info'>
                <h1>Confeitaria Personalizada</h1>
+               <p>12.345.678/0001-12</p>
                <p>Meu Endereço, 000 - Cidade - Estado</p>
+               <p>21 99999-0099</p>
             </div>
         </div> 
            
